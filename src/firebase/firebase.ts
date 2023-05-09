8e1d7077f82bcbac14130d3eac0d8555e31ac3d2
Koray Akpinar <akpinarkoray@gmail.com>
2023-05-09 11:49:27 -0400

    api key stored

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -5,12 +5,12 @@ import { getAuth } from "firebase/auth";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDrPyvkgmSSZDHWSzHMMP6krtOCn0plWBw",
-  authDomain: "appointment-dev-f066f.firebaseapp.com",
-  projectId: "appointment-dev-f066f",
-  storageBucket: "appointment-dev-f066f.appspot.com",
-  messagingSenderId: "489213295265",
-  appId: "1:489213295265:web:0455bb2f012c9b19e14ee0"
+  apiKey: process.env.REACT_APP_APPOINTAPP_APIKEY,
+  authDomain: process.env.REACT_APP_APPOINTAPP_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_APPOINTAPP_PROJECTID,
+  storageBucket: process.env.REACT_APP_APPOINTAPP_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_APPOINTAPP_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_APPOINTAPP_APPID
 };
 
 const app = initializeApp(firebaseConfig);
